8a08e5d61c73b7eace8049fd2db5342d57733f55
flawl1ss <flawl1ss.haha@gmail.com>
2022-05-28 19:04:49 +0300

    first js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// let d1 = [45, 78, 10, 3]
+// d1[7] = 100
+// console.log (d1)
+// console.log(d1[6,7])
+
+
+// let d2 = [45, 78, 10, 3]
+// let sum2 = 0
+// for(var i = 0; i < d2.length; i++) {
+//     sum2 += d2[i]
+// }
+// console.log(sum2)
+
+
+// let d3 = [45, 78, 10, 3]
+// d3.push(100)
+// let sum3 = 0
+// for(let i = 0; i < d3.length; i++ ) {
+//     sum3 += d3[i]
+// }
+// console.log(sum3)
+
+
